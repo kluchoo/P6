312c6584a5526ed205febf65b96998321d5a2c62
kluchoo <dominikzucho@gmail.com>
2025-01-15 21:04:44 +0100

    Przechowywanie danych na stercie

--- main.cpp
+++ main.cpp
@@ -2,19 +2,14 @@
 #include <fstream>
 #include "data.hpp"
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
 int main() {
     std::ifstream file("Chart Export.csv");
     std::string line;
-    std::string time;
-    double autoconsumption;
-    double eksport;
-    double import;
-    double conscription;
-    double production;
-    Data data;
+    std::vector<Data*> dataVector;
     while (std::getline(file, line)) {
         std::stringstream ss(line);
         std::string time;
@@ -30,9 +25,15 @@ int main() {
         std::getline(ss, conscription, ',');
         std::getline(ss, production, ',');
 
-        data.setParameters(time, autoconsumption, eksport, import, conscription, production);
-
-        data.printData();
+        Data* data = new Data();
+        data->setParameters(time, autoconsumption, eksport, import, conscription, production);
+        dataVector.push_back(data);
     }
+
+    // Clean up allocated memory
+    for (Data* data : dataVector) {
+        delete data;
+    }
+
     return 0;
 }
