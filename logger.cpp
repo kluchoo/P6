25c0e86d775521eaad6079a3b5921dbe22a59273
Stygus <kuba.tokarczyk@onet.eu>
2025-01-17 13:19:27 +0100

    Dodanie funkcji

--- logger.cpp
+++ logger.cpp
@@ -3,26 +3,13 @@
 #include <ctime>
 #include <stdexcept>
 
-Logger::Logger(const std::string& filename) {
-    // Generowanie nazwy pliku na podstawie aktualnej daty i godziny
-    std::time_t now = std::time(nullptr);
-    char buf[80];
-    std::strftime(buf, sizeof(buf), "%Y-%m-%d_%H-%M-%S", std::localtime(&now));
-    std::string fullFilename = filename + "_" + buf + ".log";
-
-    // Otwarcie pliku logu
-    logFile.open(fullFilename, std::ios::out | std::ios::app);
-    if (!logFile.is_open()) {
-        throw std::runtime_error("Nie można otworzyć pliku logu: " + fullFilename);
-    }
-}
-
+//tu
 Logger::~Logger() {
     if (logFile.is_open()) {
         logFile.close();
     }
 }
-
+// otwieranie oraz zamykanie pliku
 void Logger::log(const std::string& message) {
     if (logFile.is_open()) {
         // Dodanie daty i godziny do komunikatu
